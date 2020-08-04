15f470091e5e10468cba014f2313b655954b2469
André Lusegardis <lusegardis@gmail.com>
2020-08-04 11:57:35 -0300

    1.12.40 time

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,23 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.send('Hello World');
+app.use(express.json());
+
+/**Métodos
+ * GET: Buscar ou listar informação
+ * POST: Criar nova informação
+ * PUT: Atualizar informação existente
+ * DELETE: Deletar informação existente
+*/
+
+// Corpo (Request body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+// http://localhost:3333/users
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World'});
 });
 
-// localhost:3333
 
-app.listen(3333);
+app.listen(3333); //localhost:3333

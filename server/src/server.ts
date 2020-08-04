8ef8dff8da9937cf0f6ba9130d536817c6e4b451
André Lusegardis <lusegardis@gmail.com>
2020-08-04 10:42:29 -0300

    Hello World created with server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    return response.send('Hello World');
+});
+
+// localhost:3333
+
+app.listen(3333);
